39c46bf8d98c42e51a53e1a385e176331eec841b
chenshuai <384619043@qq.com>
2018-12-03 14:30:50 +0800

    使用bcrypt进行密码加密和mogoose建立数据库模型

---
+++ service/database/schema/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+let ObjectId = Schema.Types.ObjectId;
+const bcrypt = require("bcryptjs");
+const SALT_WORK_FACTOR = 10;
+
+// 创建UserSchema
+const UserSchema = new Schema({
+  UserId: ObjectId,
+  userName: {
+    unique: true,
+    type: String
+  },
+  password: String,
+  createAt: {
+    type: Date,
+    default: Date.now()
+  },
+  lastLoginAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+UserSchema.pre("save", function(next) {
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+      this.password = hash;
+      next();
+    });
+  });
+});
+
+// 发布模型
+mongoose.model("User", UserSchema);
--- service/index.js
+++ service/index.js
@@ -1,9 +1,22 @@
 const Koa = require("koa");
 const app = new Koa();
-const { connect } = require("./database/init");
+const { connect, initSchemas } = require("./database/init");
+const mongoose = require("mongoose");
 
 (async () => {
   await connect();
+  initSchemas();
+  const User = mongoose.model("User");
+  let oneUser = new User({
+    userName: "cs3",
+    password: "123456"
+  });
+  oneUser.save().then(() => {
+    console.log("插入成功");
+  });
+
+  let user = await User.findOne({ userName: "cs" });
+  console.log(user);
 })();
 
 app.use(async ctx => {
